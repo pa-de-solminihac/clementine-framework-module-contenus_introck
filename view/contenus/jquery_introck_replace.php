d6bb212188f21bcc5e05c2f49a496576de6c1cd0
Pierre-Alexis de Solminihac <pa@solminihac.fr>
2013-02-05 00:40:32 +0100

    restoring contenus_introck-1.2 branch

--- view/contenus/jquery_introck_replace.php
+++ view/contenus/jquery_introck_replace.php
@@ -1,10 +1,13 @@
+<?php
+$request = $this->getRequest();
+?>
 <script type="text/javascript">
     jQuery('textarea.editor').ckeditor(
         function () {}, 
         {
             language: 'fr',
             contentsCss: ['<?php echo __WWW_ROOT_SITE__; ?>/skin/css/style.css', '<?php echo __WWW_ROOT_SITE__; ?>/skin/css/style_surcharges.css'],
-            contentsLanguage: '<?php echo $request->LANG; ?>',
+            contentsLanguage: '<?php echo $request['LANG']; ?>',
             scayt_autoStartup: false
         }
     ); 
