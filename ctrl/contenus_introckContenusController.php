923e68fa3004b03dee233cc2824d685d9cf859af
Pierre-Alexis de Solminihac <pa@solminihac.fr>
2013-02-05 00:40:27 +0100

    restoring contenus_introck-1.1 branch

--- ctrl/contenus_introckContenusController.php
+++ ctrl/contenus_introckContenusController.php
@@ -17,29 +17,25 @@ class contenus_introckContenusController extends contenus_introckContenusControl
      * @access public
      * @return void
      */
-    function editcontenuAction ($request)
+    function editcontenuAction ()
     {
         // recupere le contenu du script a injecter dans le footer
         $script = $this->getBlockHtml('contenus/jquery_introck_replace');
         // charge les javascripts necessaires
-        if (Clementine::$config['module_jstools']['use_google_cdn']) {
-            $this->getModel('cssjs')->register_js('jquery', array('src' => 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js'));
-        } else {
-            $this->getModel('cssjs')->register_js('jquery', array('src' => __WWW_ROOT_JSTOOLS__ . '/skin/jquery/jquery.min.js'));
-        }
+        $this->getModel('cssjs')->register_js('jquery', array('src' => 'https://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js'));
         $this->getModel('cssjs')->register_js('ckeditor', array('src' => __WWW_ROOT_JSTOOLS__ . '/skin/js/ckeditor/ckeditor.js'));
         $this->getModel('cssjs')->register_js('jquery.ckeditor', array('src' => __WWW_ROOT_JSTOOLS__ . '/skin/js/ckeditor/adapters/jquery.js'));
         // javascript de configuration de ckeditor
         $this->getModel('cssjs')->register_foot('jquery.ckeditor.replace', $script);
         // execute le controleur normal
-        return parent::editcontenuAction($request);
+        return parent::editcontenuAction();
     }
 
     /**
     * Function : valid_clementine_cms_contenu_html_introckAction() 
     * 
     */
-    function valid_clementine_cms_contenu_html_introckAction($request) 
+    function valid_clementine_cms_contenu_html_introckAction() 
     {
         if ($this->getModel('users')->needPrivilege('manage_contents')) {
             $ns = $this->getModel('fonctions');
@@ -54,7 +50,7 @@ class contenus_introckContenusController extends contenus_introckContenusControl
                 $contenus = $this->getModel('contenus');
                 // ajoute le contenu s'il n'existe pas deja
                 $request = $this->getRequest();
-                $lang = $request->LANG;
+                $lang = $request['LANG'];
                 if (!$id) {
                     $id = $contenus->addContenu($nom, $type_content, $id_zone, $id_page, $lang);
                 }
