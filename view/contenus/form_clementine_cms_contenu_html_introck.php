b53b0719cd6827e5ab892636d1ecd58644f8a5ca
Pierre-Alexis de Solminihac <pa@solminihac.fr>
2013-02-05 00:40:23 +0100

    restoring contenus_introck-1.0 branch

--- view/contenus/form_clementine_cms_contenu_html_introck.php
+++ view/contenus/form_clementine_cms_contenu_html_introck.php
@@ -3,10 +3,6 @@
             <textarea name="contenu_html"><?php 
     if (isset($data['contenu_html'])) { 
         echo $this->getModel('fonctions')->htmlentities($data['contenu_html']); 
-    } else {
-        if ($this->canGetBlock('contenus_introck/default_contenu_html')) {
-            $this->getBlock('contenus_introck/default_contenu_html', $data);
-        }
     }
 ?></textarea>
         </p>
@@ -15,10 +11,6 @@
             <textarea class="editor" name="contenu_html_introck"><?php 
     if (isset($data['contenu_html_introck'])) { 
         echo $this->getModel('fonctions')->htmlentities($data['contenu_html_introck']); 
-    } else {
-        if ($this->canGetBlock('contenus_introck/default_contenu_html_introck')) {
-            $this->getBlock('contenus_introck/default_contenu_html_introck', $data);
-        }
     }
 ?></textarea>
         </p>
